34d96814ca6c0b79da97a67e4af12e128bc97337
aditya <dev@chimeracoder.net>
2013-09-16 23:06:42 -0400

    Sleep for 60 seconds, and separate logging from printed output
    * Log messages are sent to STDERR
    * Output is sent to STDOUT

--- logger.go
+++ logger.go
@@ -1,44 +1,52 @@
 package main
 
 import (
-    "github.com/ChimeraCoder/anaconda"
-    "log"
-    "net/url"
-    "time"
+	"fmt"
+	"github.com/ChimeraCoder/anaconda"
+	"log"
+	"net/url"
+	"time"
 )
 
+func main() {
+	anaconda.SetConsumerKey(TWITTER_CONSUMER_KEY)
+	anaconda.SetConsumerSecret(TWITTER_CONSUMER_SECRET)
+	api := anaconda.NewTwitterApi(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 
-func main(){
-    anaconda.SetConsumerKey(TWITTER_CONSUMER_KEY)
-    anaconda.SetConsumerSecret(TWITTER_CONSUMER_SECRET)
-    api := anaconda.NewTwitterApi(TWITTER_ACCESS_TOKEN,TWITTER_ACCESS_TOKEN_SECRET)
+	v := url.Values{}
 
-    v := url.Values{}
+	users := []anaconda.TwitterUser{}
 
-    users := []anaconda.TwitterUser{}
+	next_cursor := "-1"
+	for {
+		v.Set("cursor", next_cursor)
+		c, err := api.GetFollowersList(v)
 
-    next_cursor := "-1"
-    for {
-        v.Set("cursor", next_cursor)
-        c, err := api.GetFollowersList(v)
-        if err != nil{
-            panic(err)
-        }
+		//TODO distinguish between rate limiting errors and other errors
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+			time.Sleep(90 * time.Second)
+			continue //Retry
+		}
 
-        users = append(users, c.Users...)
+		users = append(users, c.Users...)
+		log.Printf("Appended %d users", len(c.Users))
+		go func(us []anaconda.TwitterUser) {
+			for _, user := range us {
+				log.Printf("Appended %+v", *user.Screen_name)
+			}
+		}(c.Users)
 
-        next_cursor = c.Next_cursor_str
-        if next_cursor == "0" {
-            break
-        } else {
-            log.Printf("Calling again with cursor %v", next_cursor)
-        }
-        time.Sleep(5 * time.Second)
-    }
+		next_cursor = c.Next_cursor_str
+		if next_cursor == "0" {
+			break
+		} else {
+			log.Printf("Calling again with cursor %v", next_cursor)
+		}
+		time.Sleep(60 * time.Second)
+	}
 
-
-
-    for _, user := range users {
-        log.Printf("%+v", *user.Screen_name)
-    }
+	for _, user := range users {
+		fmt.Printf("%+v", *user.Screen_name)
+	}
 }
